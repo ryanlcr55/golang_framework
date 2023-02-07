9a0d0a5b21175b4cc0a23ddb4715798f9f990622
Ryan <lcrob55@gmail.com>
2023-02-07 17:21:23 +0800

    chore: post status enums

--- internal/adapters/gormRepo/post.go
+++ internal/adapters/gormRepo/post.go
@@ -11,15 +11,15 @@ import (
 )
 
 type PostModel struct {
-	ID        uint64         `gorm:"primaryKey;autoIncrement;"`
-	No        string         `gorm:"type:varchar(50);not null;uniqueIndex"`
-	UserNo    string         `gorm:"type:varchar(50);not null;index"`
-	Status    string         `gorm:"type:enum('enable', 'disable');default:'enable';not null;"`
-	Title     string         `gorm:"type:varchar(255);not null;"`
-	Content   string         `gorm:"type:varchar(255);not null"`
-	ShowAt    datatypes.Date `gorm:"type:date;not null;unique Index"`
-	CreatedAt time.Time      `gorm:"type:datetime;autoCreateTime;"`
-	UpdatedAt time.Time      `gorm:"type:datetime;autoCreateTime;"`
+	ID        uint64              `gorm:"primaryKey;autoIncrement;"`
+	No        string              `gorm:"type:varchar(50);not null;uniqueIndex"`
+	UserNo    string              `gorm:"type:varchar(50);not null;index"`
+	Status    entities.PostStatus `gorm:"type:enum('enable', 'disable');default:'enable';not null;"`
+	Title     string              `gorm:"type:varchar(255);not null;"`
+	Content   string              `gorm:"type:varchar(255);not null"`
+	ShowAt    datatypes.Date      `gorm:"type:date;not null;unique Index"`
+	CreatedAt time.Time           `gorm:"type:datetime;autoCreateTime;"`
+	UpdatedAt time.Time           `gorm:"type:datetime;autoCreateTime;"`
 }
 
 type PostRepo struct {
--- internal/app/entities/post.go
+++ internal/app/entities/post.go
@@ -2,13 +2,18 @@ package entities
 
 import "time"
 
+type PostStatus int
+
+const PostStatusEnable PostStatus = 1
+const PostStatusDisable PostStatus = 2
+
 type Post struct {
 	ID        uint64
 	No        string
 	UserNo    string
 	Title     string
 	Content   string
-	Status    string
+	Status    PostStatus
 	ShowAt    time.Time
 	CreatedAt time.Time
 	UpdatedAt time.Time
