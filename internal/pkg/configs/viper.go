6d7d1802632868a05c7cdfa2feb5ec91065fe7dd
Ryan <lcrob55@gmail.com>
2023-02-25 18:57:22 +0800

    fix: viper get env file path

--- internal/pkg/configs/viper.go
+++ internal/pkg/configs/viper.go
@@ -11,8 +11,7 @@ func NewServerConfig() *configs.Configs {
 	log.Println("Configs is initializing...")
 	config := configs.Configs{}
 	setDefault(&config)
-	viper.AddConfigPath("../../")
-	viper.SetConfigFile(".env")
+	viper.SetConfigFile("../../../.env")
 	viper.AutomaticEnv()
 
 	if err := viperConfigRead(&config); err != nil {
